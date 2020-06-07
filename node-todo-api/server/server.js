70393e645b7afdd6c97df7140f86c4928d4609da
santanu12saha <saha.santanu0217@gmail.com>
2020-06-07 19:32:46 +0530

    Add test cases for DELETE /todos/id.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -47,7 +47,7 @@ app.delete('/todos/:id', (req, res) => {
     }
     todoService.removeTodoById(id).then((todo) => {
         if(!todo){
-            return res.send(404).send();
+            return res.status(404).send();
         }
         res.send({todo});
     }, (err) => {
